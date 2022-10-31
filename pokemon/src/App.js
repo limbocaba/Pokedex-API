9148e72169b0b9364e7141a329ad3c4e64fb5f17
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-31 16:32:55 -0400

    images now combined with pokemon json

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,6 +1,26 @@
 import './App.css';
+import { useEffect, useState } from "react"
+import pokeData from "./data/poke.json"
+import pokeImages from "./data/images.json"
 
 function App() {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    let filteredPokemon = pokeData.map(pokemon => {
+      let foundImg = pokeImages.find(img => {
+        return img.name.name === pokemon.name
+      })
+
+      if (foundImg) {
+        pokemon.image = foundImg.artwork.image
+        return pokemon
+      }
+    })
+
+    setPokemons(filteredPokemon)
+  }, [])
+  
   return (
     <div className="App">
      <h2>I am the App.js file look at me </h2>
